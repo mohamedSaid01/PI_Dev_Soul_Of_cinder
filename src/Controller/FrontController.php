96836a44205ce68cb722526a107ed0f9f699d86d
Mohamed Said Boubaker <mohamedsaidboubaker10@gmail.com>
2025-02-04 16:05:22 +0100

    Front et Back office

---
+++ src/Controller/FrontController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class FrontController extends AbstractController
+{
+    #[Route('/', name: 'display_front')]
+    public function index(): Response
+    {
+        return $this->render('front/index.html.twig'
+        );
+    }
+}
